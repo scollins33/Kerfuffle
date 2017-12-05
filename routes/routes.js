726c6cf7c66937cfc2d5bcfb01da305a894c1900
Kimberly <kowens4@elon.edu>
2017-12-04 19:08:31 -0500

    tried it seans way

--- routes/routes.js
+++ routes/routes.js
@@ -4,23 +4,32 @@ const db = require('../models/db');
 // Routes
 // =============================================================
 module.exports = function(app) {
+    let randomQs = [];
+    function tenQs() {
+        // GET route for getting all of the todos
+        app.get("/api/qs", function (req, res) {
+            // findAll returns all entries for a table when used with no options
+            db.questions.findAll({
 
-    // GET route for getting all of the todos
-    app.get("/api/qs", function (req, res) {
-        // findAll returns all entries for a table when used with no options
-        db.questions.findAll({
-
-                limit:  Math.floor(Math.random() * 500),
-
-        }).then(function (dbquestions) {
-
-
-            // We have access to the todos as an argument inside of the callback function
-
-            res.json(dbquestions);
+                where: Math.floor(Math.random() * 500),
 
 
+            }).then(function (dbquestions) {
+
+
+                // We have access to the todos as an argument inside of the callback function
+
+                res.json(dbquestions);
+
+
+            });
         });
-    });
-}
+    }
+
+    let firstQ = Math.floor(Math.random(tenQs(10)));
+
+    randomQs.push(firstQ);
+    console.log('what is this',randomQs)
+
+};
 
